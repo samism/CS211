a8697d5a6a6b63541de7fcfa71823ea521ad1380
Sam Ismail <sam@samism.net>
2015-09-30 11:40:40 -0500

    Initial commit for lab3 of cs 211 at UIC.

---
+++ CS211/sismai4lab3.c
@@ -0,0 +1,13 @@
+//
+// sismai4lab3.c
+//  CS211
+//
+//  Created by Sameer Ismail on 9/30/15.
+//  Copyright © 2015 Sameer Ismail. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+}
